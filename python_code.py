e16db827fdc5a8ab62374e24b1aef6f7694fab15
Carter Hughes <chughe97@students.kennesaw.edu>
2023-11-10 11:57:01 -0500

    Install pandas and import data set

--- python_code.py
+++ python_code.py
@@ -1,1 +1,20 @@
 ## Main Code Page for MEDINFOPRO ##
+## Instal Pandas ##
+pip install pandas
+
+## Import data set ##
+import pandas as pd
+## Import os if using join to bring in file ##
+import os
+
+## Specify the file path to define excel_file_path ##
+excel_file_path = 'path/to/your/excel/file.xlsx'
+
+## OR ##
+
+## Create file name and path then join ##
+fileName='csv or excel file name'
+filePath=r'C:\Users\Carter\Downloads'
+file=os.path.join(filePath,fileName) 
+
+
