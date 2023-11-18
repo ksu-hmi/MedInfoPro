103914a5ad9f5bc04bafdb02b9aa3d1adc50c1ab
jihedio1 <jihedio1@students.kennesaw.edu>
2023-11-18 08:33:03 -0500

    Update MedInfoProDemo_Code.py
    Created Dataset file into dictionary

--- MedInfoProDemo_Code.py
+++ MedInfoProDemo_Code.py
@@ -4,3 +4,9 @@ import pandas as pd
 
 df = pd.read_excel(r"C:\Users\rober\OneDrive\Documents\GitHub\MedInfoPro\Data\Medicine_description.xlsx")
 
+# Create Dataset into dictionary
+
+med_dict = df.to_dict()
+
+
+
