f5dcbd5f3afe8456a11df73b66972fe8d1a4f449
Carter Hughes <chughe97@students.kennesaw.edu>
2023-12-02 14:42:17 -0500

    final code

--- RoughtDraft3.py
+++ RoughtDraft3.py
@@ -8,6 +8,21 @@ class DrugInfoApp:
         self.root = root
         self.root.title("Drug Information App")
 
+        # Load Excel file into a DataFrame
+        try:
+            self.df = pd.read_excel(r"C:\Users\Carter\Documents\GitHub\MedInfoPro\Data\Medicine_description.xlsx")
+        except pd.errors.EmptyDataError:
+            messagebox.showerror("Error", "The Excel file is empty.")
+            return
+        except FileNotFoundError:
+            messagebox.showerror("Error", "Excel file not found.")
+            return
+
+        print(self.df.columns)
+
+        # Remove duplicates based on the 'drug_name' column
+        self.df = self.df.drop_duplicates(subset='drug_name')
+
         # Variables to store user input
         self.name_var = tk.StringVar()
         self.dob_var = tk.StringVar()
@@ -16,6 +31,7 @@ class DrugInfoApp:
 
         # Create GUI components
         self.create_widgets()
+
 
     def create_widgets(self):
         # Labels and Entry widgets for user input
@@ -35,35 +51,25 @@ class DrugInfoApp:
         ttk.Button(self.root, text="Get Drug Information", command=self.get_drug_info).grid(row=4, column=0, columnspan=2, pady=10)
 
     def get_drug_info(self):
-        # Load Excel file into a DataFrame
-        try:
-            df = pd.read_excel(r"C:\Users\Carter\Documents\GitHub\MedInfoPro\Data\Medicine_description (1).xlsx")
-        except pd.errors.EmptyDataError:
-            messagebox.showerror("Error", "The Excel file is empty.")
-            return
-        except FileNotFoundError:
-            messagebox.showerror("Error", "Excel file not found.")
-            return
-
         # Get user input
         medication_wanted = self.medication_var.get()
 
-        # Search for drug information based on the medication wanted
-        matching_drugs = df[df['drug_name'].str.contains(medication_wanted, case=False)]
+        # Convert DataFrame to dictionary
+        drug_dict = self.df.set_index('drug_name').to_dict(orient='index')
 
-        if matching_drugs.empty:
-            messagebox.showinfo("Information", "No matching drugs found.")
-        else:
-            # Display drug information
-            drug_info = matching_drugs.iloc[0]
+        # Search for drug information based on the medication wanted
+        if medication_wanted in drug_dict:
+            drug_info = drug_dict[medication_wanted]
             messagebox.showinfo("Drug Information",
-                                f"Drug Name: {drug_info['drug_name']}\n"
+                                f"Drug Name: {medication_wanted}\n"
                                 f"Description: {drug_info['description']}\n"
                                 f"Quantity: {self.quantity_var.get()}\n"
                                 f"Categories: {drug_info['categories']}\n"
-                                f"Price: {drug_info['price']}\n"
+                                f"Price: {drug_info['price ']}\n"
                                 f"Drug Interactions: {drug_info['drug_interactions']}\n"
-                                f"Side Effects: {drug_info['side_effects']}")
+                                f"Side Effects: {drug_info['side_effects ']}")
+        else:
+            messagebox.showinfo("Information", f"No information found for {medication_wanted}.")
 
     def validate_quantity(self, new_value):
         # Validate that quantity is a positive integer
